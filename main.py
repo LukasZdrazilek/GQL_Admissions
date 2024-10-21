d0a6fc1b4fd7458602162709eb6b3eb6a9195ed4
LukasZdrazilek <lukas.zdrazilek@unob.cz>
2024-10-21 09:02:33 +0200

    vytvoreni projects branch

---
+++ main.py
@@ -0,0 +1,159 @@
+import os
+from contextlib import asynccontextmanager
+import asyncio
+import socket
+import logging
+import logging.handlers
+from fastapi import FastAPI, Request
+from strawberry.asgi import GraphQL
+
+from uoishelpers.gqlrouter import MountGuardedGQL
+
+from src.DBDefinitions import startEngine, ComposeConnectionString
+from src.DBFeeder import initDB
+from src.Dataloaders import createLoaders
+from src.GraphTypeDefinitions import schema
+
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s.%(msecs)03d\t%(levelname)s:\t%(message)s', 
+    datefmt='%Y-%m-%dT%I:%M:%S')
+SYSLOGHOST = os.getenv("SYSLOGHOST", None)
+if SYSLOGHOST is not None:
+    [address, strport, *_] = SYSLOGHOST.split(':')
+    assert len(_) == 0, f"SYSLOGHOST {SYSLOGHOST} has unexpected structure, try `localhost:514` or similar (514 is UDP port)"
+    port = int(strport)
+    my_logger = logging.getLogger()
+    my_logger.setLevel(logging.INFO)
+    handler = logging.handlers.SysLogHandler(address=(address, port), socktype=socket.SOCK_DGRAM)
+    #handler = logging.handlers.SocketHandler('10.10.11.11', 611)
+    my_logger.addHandler(handler)
+
+## Zabezpecuje prvotni inicializaci DB a definovani Nahodne struktury pro "Univerzity"
+# from gql_workflow.DBFeeder import createSystemDataStructureRoleTypes, createSystemDataStructureGroupTypes
+
+connectionString = ComposeConnectionString()
+
+
+def singleCall(asyncFunc):
+    """Dekorator, ktery dovoli, aby dekorovana funkce byla volana (vycislena) jen jednou. Navratova hodnota je zapamatovana a pri dalsich volanich vracena.
+    Dekorovana funkce je asynchronni.
+    """
+    resultCache = {}
+
+    async def result():
+        if resultCache.get("result", None) is None:
+            resultCache["result"] = await asyncFunc()
+        return resultCache["result"]
+
+    return result
+
+@singleCall
+async def RunOnceAndReturnSessionMaker():
+    """Provadi inicializaci asynchronniho db engine, inicializaci databaze a vraci asynchronni SessionMaker.
+    Protoze je dekorovana, volani teto funkce se provede jen jednou a vystup se zapamatuje a vraci se pri dalsich volanich.
+    """
+    print(f'starting engine for "{connectionString}"')
+
+    import os
+    makeDrop = os.environ.get("DEMO", "") == "True"
+    result = await startEngine(
+        connectionstring=connectionString, makeDrop=makeDrop, makeUp=True
+    )
+
+    print(f"initializing system structures")
+
+    ###########################################################################################################################
+    #
+    # zde definujte do funkce asyncio.gather
+    # vlozte asynchronni funkce, ktere maji data uvest do prvotniho konzistentniho stavu
+    asyncio.create_task(initDB(result))
+    # await initDB(result)
+    # await asyncio.gather( # concurency running :)
+    # sem lze dat vsechny funkce, ktere maji nejak inicializovat databazi
+    # musi byt asynchronniho typu (async def ...)
+    # createSystemDataStructureRoleTypes(result),
+    # createSystemDataStructureGroupTypes(result)
+    # )
+
+    ###########################################################################################################################
+    print(f"all done")
+    return result
+
+async def get_context(request: Request):
+    asyncSessionMaker = await RunOnceAndReturnSessionMaker()
+        
+    #from src.Dataloaders import createLoadersContext, createUgConnectionContext
+    from src.Dataloaders import createLoadersContext
+    context = createLoadersContext(asyncSessionMaker)
+    # i = Item(query = "")
+    # # i.query = ""
+    # # i.variables = {}
+    # logging.info(f"before sentinel current user is {request.scope.get('user', None)}")
+    # await sentinel(request, i)
+    # logging.info(f"after sentinel current user is {request.scope.get('user', None)}")
+    # connectionContext = createUgConnectionContext(request=request)
+    # result = {**context, **connectionContext}
+    result = {**context}
+    result["request"] = request
+    # result["user"] = request.scope.get("user", None)
+    logging.info(f"context created {result}")
+    return result
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    initizalizedEngine = await RunOnceAndReturnSessionMaker()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+DEMO = os.getenv("DEMO", None)
+MountGuardedGQL(app, schema=schema, get_context=get_context, DEMO=os.getenv("DEMO", None))
+
+# region ENV setup tests
+def envAssertDefined(name, default=None):
+    result = os.getenv(name, None)
+    assert result is not None, f"{name} environment variable must be explicitly defined"
+    return result
+
+DEMO = envAssertDefined("DEMO", None)
+JWTPUBLICKEYURL = envAssertDefined("JWTPUBLICKEYURL", None)
+JWTRESOLVEUSERPATHURL = envAssertDefined("JWTRESOLVEUSERPATHURL", None)
+
+assert (DEMO in ["True", "true", "False", "false"]), "DEMO environment variable can have only `True` or `False` values"
+DEMO = DEMO in ["True", "true"]
+
+if DEMO:
+    print("####################################################")
+    print("#                                                  #")
+    print("# RUNNING IN DEMO                                  #")
+    print("#                                                  #")
+    print("####################################################")
+
+    logging.info("####################################################")
+    logging.info("#                                                  #")
+    logging.info("# RUNNING IN DEMO                                  #")
+    logging.info("#                                                  #")
+    logging.info("####################################################")
+else:
+    print("####################################################")
+    print("#                                                  #")
+    print("# RUNNING DEPLOYMENT                               #")
+    print("#                                                  #")
+    print("####################################################")
+
+    logging.info("####################################################")
+    logging.info("#                                                  #")
+    logging.info("# RUNNING DEPLOYMENT                               #")
+    logging.info("#                                                  #")
+    logging.info("####################################################")    
+
+logging.info(f"DEMO = {DEMO}")
+logging.info(f"SYSLOGHOST = {SYSLOGHOST}")
+logging.info(f"JWTPUBLICKEYURL = {JWTPUBLICKEYURL}")
+logging.info(f"JWTRESOLVEUSERPATHURL = {JWTRESOLVEUSERPATHURL}")
+# endregion
+
+print("All initialization is done")
+
