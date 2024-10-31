76a130396ee98f8affa0fea0b42bf3cc0c1b8f9a
jakubbvagner <jakubvagner2002@gmail.com>
2024-10-31 12:34:09 +0100

    Addition of externals.py, GraphPermission.py and GraphResolvers.py, _GraphResolvers.py eddited

--- src/GraphTypeDefinitions/_GraphResolvers.py
+++ src/GraphTypeDefinitions/_GraphResolvers.py
@@ -5,9 +5,7 @@ import typing
 from src.GraphPermissions import OnlyForAuthentized
 
 UserGQLModel = typing.Annotated["UserGQLModel", strawberry.lazy(".externals")]
-AdmissionGQLModel = typing.Annotated["AdmissionGQLModel", strawberry.lazy(".admissions")]
-ExamTypeGQLModel = typing.Annotated["ExamTypeGQLModel", strawberry.lazy(".exams")]
-DisciplineTypeGQLModel = typing.Annotated["DisciplineTypeGQLModel", strawberry.lazy(".disciplines")]
+GrantingGQLModel = typing.Annotated["GrantingGQLModel", strawberry.lazy(".externals")]
 
 @strawberry.field(description="Entity primary key", permission_classes=[OnlyForAuthentized()])
 def resolve_id(self) -> uuid.UUID:
@@ -33,6 +31,26 @@ def resolve_enddate(self) -> datetime.date:
 def resolve_valid(self) -> bool:
     return self.valid
 
+async def resolve_user(info, user_id):
+    from .externals import UserGQLModel
+    result = None if user_id is None else await UserGQLModel.resolve_reference(info, user_id)
+    return result
+
+@strawberry.field(description="""User ID """, permission_classes=[OnlyForAuthentized()])
+async def resolve_user_id(self, info) -> typing.Optional["UserGQLModel"]:
+    return await resolve_user(info, self.user_id)
+
+#################################################### TEST GRANTING #####################################################
+async def resolve_program(info, program_id):
+    from .externals import GrantingGQLModel
+    result = None if program_id is None else await GrantingGQLModel.resolve_reference(info, program_id)
+    return result
+
+@strawberry.field(description="""Granting ID """, permission_classes=[OnlyForAuthentized()])
+async def resolve_program_id(self, info) -> typing.Optional["GrantingGQLModel"]:
+    return await resolve_user(info, self.program_id)
+########################################################################################################################
+
 @strawberry.field(description="Creation time", permission_classes=[OnlyForAuthentized()])
 def resolve_created(self) -> typing.Optional[datetime.datetime]:
     return self.created
@@ -48,40 +66,3 @@ async def resolve_createdby(self, info) -> typing.Optional[UserGQLModel]:
 @strawberry.field(description="User who last changed the entity", permission_classes=[OnlyForAuthentized()])
 async def resolve_changedby(self, info) -> typing.Optional[UserGQLModel]:
     return await resolve_user(info, self.changedby)
-
-@strawberry.field(description="Related admission entity", permission_classes=[OnlyForAuthentized()])
-async def resolve_admission(self, info) -> typing.Optional[AdmissionGQLModel]:
-    from .admissions import AdmissionGQLModel
-    return await AdmissionGQLModel.resolve_reference(info, self.admission_id)
-
-@strawberry.field(description="Related exam type", permission_classes=[OnlyForAuthentized()])
-async def resolve_examtype(self, info) -> typing.Optional[ExamTypeGQLModel]:
-    from .exams import ExamTypeGQLModel
-    return await ExamTypeGQLModel.resolve_reference(info, self.examtype_id)
-
-@strawberry.field(description="Related discipline type", permission_classes=[OnlyForAuthentized()])
-async def resolve_disciplinetype(self, info) -> typing.Optional[DisciplineTypeGQLModel]:
-    from .disciplines import DisciplineTypeGQLModel
-    return await DisciplineTypeGQLModel.resolve_reference(info, self.disciplinetype_id)
-
-@strawberry.type
-class AdmissionGQLModel:
-    id: uuid.UUID
-    name: str
-    name_en: typing.Optional[str]
-    description: typing.Optional[str]
-    valid: bool
-    created: datetime.datetime
-    lastchange: datetime.datetime
-    # Additional fields and relationships...
-
-@strawberry.type
-class StudentAdmissionGQLModel:
-    id: uuid.UUID
-    admission: AdmissionGQLModel
-    user_id: uuid.UUID
-    state: str
-    valid: bool
-    created: datetime.datetime
-    lastchange: datetime.datetime
-    # Additional fields and relationships...
---
+++ src/GraphTypeDefinitions/externals.py
@@ -0,0 +1,26 @@
+import strawberry
+import uuid
+
+
+@classmethod
+async def resolve_reference(cls, info: strawberry.types.Info, id: uuid.UUID): return cls(id=id)
+
+
+class BaseEternal:
+    id: uuid.UUID = strawberry.federation.field(external=True)
+
+
+@strawberry.federation.type(extend=True, keys=["id"])
+class UserGQLModel:
+    id: uuid.UUID = strawberry.federation.field(external=True)
+    resolve_reference = resolve_reference
+
+@strawberry.federation.type(extend=True, keys=["id"])
+class GrantingGQLModel:
+    id: uuid.UUID = strawberry.federation.field(external=True)
+    resolve_reference = resolve_reference
+
+@strawberry.federation.type(extend=True, keys=["id"])
+class RBACObjectGQLModel:
+    id: uuid.UUID = strawberry.federation.field(external=True)
+    resolve_reference = resolve_reference
